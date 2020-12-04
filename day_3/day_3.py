675447f460d8b449abeecef7037ea7a12d54486a
Jack Ballinger <j.ballinger@winton.com>
2020-12-04 13:50:28 +0000

    updating day_3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,30 +1,44 @@
 import numpy as np
+from os.path import abspath, dirname
+from os import getcwd
+from pathlib import Path
+
+try:
+    p = Path(__file__).parent
+    file_location = p / 'puzzle_input.txt'
+except NameError:
+    file_location = 'puzzle_input.txt'
+
 
 # read in data
-with open('puzzle_input.txt') as f:
+with open(file_location) as f:
     data = f.read().split('\n')
 
-# configure input_data
+# configure data
 n = len(data)
 new_data = [line+(n*line) for line in data]
 
+# toboggan class
+class Toboggan:
+    def __init__(self, route):
+        self.route = route
+        self.x_pos, self.y_pos, self.trees = 0,0,0
+        
+    def navigate(self, x_step, y_step):
+        while True:
+            if self.y_pos >= len(self.route):
+                break
+            if self.route[self.y_pos][self.x_pos] == '#':
+                self.trees+=1
+            self.x_pos += x_step
+            self.y_pos += y_step
+        return self.trees
+
 # part 1
-x_pos = 0
-y_pos = 0
-x_step = 3
-y_step = 1
-trees = 0
-while True:
-    if y_pos >= len(new_data):
-        break
-    if new_data[y_pos][x_pos] == '#':
-        trees+=1
-    x_pos += x_step
-    y_pos += y_step
-print(trees)
+print(Toboggan(new_data).navigate(x_step=3, y_step=1))
 
 # part 2
-pos_list = [
+slope_list = [
     (1,1),
     (3,1),
     (5,1),
@@ -32,19 +46,5 @@ pos_list = [
     (1,2)
 ]
 
-total_trees = []
-for pos in pos_list:
-    x_pos = 0
-    y_pos = 0
-    x_step = pos[0]
-    y_step = pos[1]
-    trees = 0
-    while True:
-        if y_pos >= len(new_data):
-            break
-        if new_data[y_pos][x_pos] == '#':
-            trees+=1
-        x_pos += x_step
-        y_pos += y_step
-    total_trees.append(trees)
-print(np.prod(total_trees))
+print(np.prod([Toboggan(new_data).navigate(x_step=slope[0], y_step=slope[1]) for slope in slope_list]))
+    
