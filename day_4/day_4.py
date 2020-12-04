e373ddd55127383c68c7613d835dd959d315484a
Jack Ballinger <j.ballinger@winton.com>
2020-12-04 10:41:42 +0000

    adding day 4

---
+++ day_4/day_4.py
@@ -0,0 +1,57 @@
+import re
+
+with open('puzzle_input.txt') as f:
+    data = [{y.split(':')[0]:y.split(':')[1] for y in x.replace('\n',' ').split(' ') if y.split(':')[0] != 'cid'} for x in f.read().split('\n\n')]
+    
+# part 1
+print(sum([1 if len(x) == 7 else 0 for x in data]))
+
+# part 2
+class Validator:
+    def __init__(self, passport):
+        self.passport = passport
+        
+    def _validate_birth_year(self, birth_year):
+        return (len(birth_year)==4) and (1920<=int(birth_year)<=2002)
+    
+    def _validate_issue_year(self, issue_year):
+        return (len(issue_year)==4) and (2010<=int(issue_year)<=2020)
+
+    def _validate_expiration_year(self, expiration_year):
+        return (len(expiration_year)==4) and (2020<=int(expiration_year)<=2030)
+    
+    def _validate_height(self, height):
+        if re.match('(:?^(1[5-8][0-9]|19[0-3])(cm)$|^(5[9]|[6][0-9]|7[0-6])(in)$)',height):
+            return True
+        return False
+    
+    def _validate_hair_colour(self, hair_colour):
+        if re.match('^#([0-9a-f]){6}$', hair_colour):
+            return True
+        return False
+    
+    def _validate_eye_colour(self, eye_colour):
+        return eye_colour in ['amb','blu','brn','gry','grn','hzl','oth']
+    
+    def _validate_passport_id(self, passport_id):
+        if re.match('^[0-9]{9}$', passport_id):
+            return True
+        return False
+    
+    def validate_passport(self):
+        if len(self.passport) >= 7:
+            return all(
+                    [
+                        self._validate_birth_year(self.passport['byr']),
+                        self._validate_issue_year(self.passport['iyr']),
+                        self._validate_expiration_year(self.passport['eyr']),
+                        self._validate_height(self.passport['hgt']),
+                        self._validate_hair_colour(self.passport['hcl']),
+                        self._validate_eye_colour(self.passport['ecl']),
+                        self._validate_passport_id(self.passport['pid'])
+                    ]
+                ) and all(
+                        elem in passport for elem in ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+                )
+        return False
+print(sum([Validator(passport).validate_passport() for passport in data]))
