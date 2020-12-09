ea33285b59d9adbaf0c768ed7c7bbaca28ba6717
Jack Ballinger <j.ballinger@winton.com>
2020-12-09 14:28:09 +0000

    better answer still

--- python_files/day_9.py
+++ python_files/day_9.py
@@ -7,29 +7,28 @@ with open(get_file_location(day=9)) as f:
     data = [int(x) for x in f.read().split('\n')]
     
 class EncodingFixer:
-    def __init__(self, input_data, preamble, max_combinations):
+    def __init__(self, input_data, preamble):
         self.input_data = input_data
         self.preamble = preamble
-        self.max_combinations = max_combinations
         
     def find_broken_row(self):
         for i, row in enumerate(self.input_data[self.preamble:]):
             if not [x for x in list(itertools.combinations(self.input_data[i:i+self.preamble],2)) if sum(x) == row]:
                 return row
     
-    def find_correct_combinations(self):
+    def find_contiguous_set(self):
         broken_row = self.find_broken_row()
-        for i, row in enumerate(self.input_data[:self.input_data.index(broken_row)]):
-            for n_combinations in range(1, self.max_combinations+1):
-                for x in itertools.combinations(self.input_data[i:i+n_combinations], n_combinations):
-                    if sum(x) == broken_row:
-                        return x
-    
+        for i in range(len(self.input_data)):
+            sums = [sum(self.input_data[i:x]) for x in range(len(self.input_data)-i)]
+            if broken_row in sums:
+                position = sums.index(broken_row)
+                return self.input_data[i:position]
+            
     def find_encryption_weakness(self):
-        correct_combinations = self.find_correct_combinations()
-        return max(correct_combinations) + min(correct_combinations)
+        contiguous_set = self.find_contiguous_set()
+        return min(contiguous_set) + max(contiguous_set)
     
-encoding_fixer = EncodingFixer(data, preamble=25, max_combinations=20)
+encoding_fixer = EncodingFixer(data, preamble=25)
 
 # part 1
 broken_row = encoding_fixer.find_broken_row()
