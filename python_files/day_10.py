89dc69e58f11d810b42101c632b3bbdabd2df157
Jack Ballinger <j.ballinger@winton.com>
2020-12-11 07:39:22 +0000

    changing default dict to counter

--- python_files/day_10.py
+++ python_files/day_10.py
@@ -18,8 +18,7 @@ differences[3]+=1
 print(differences[1] * differences[3]) # 2470
 
 # part 2
-counter_dict = defaultdict(int)
-counter_dict[0] = 1
+counter_dict = Counter((0,))
 for i, row in enumerate(data):
     counter_dict[row] = sum([counter_dict[y] for y in range(row-3, row) if y in data or y == 0])
 print(counter_dict[data[-1]]) # 1973822685184
