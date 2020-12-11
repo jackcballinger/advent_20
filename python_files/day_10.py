1aa093ea3eec11edad685cac8161634a7bc43965
Jack Ballinger <j.ballinger@winton.com>
2020-12-11 18:44:41 +0000

    updating day_10

--- python_files/day_10.py
+++ python_files/day_10.py
@@ -1,24 +1,21 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 from utils import get_file_location
 
 # data load
 with open(get_file_location(day=10)) as f:
-    data = [int(x) for x in f.read().split('\n')]
-data = list(set(data))
+    data = list(set([int(x) for x in f.read().split('\n')]))
 
 # part 1
-i, differences = 0, defaultdict(int)
-look_ahead = 3
-for x in set(data):
+i, differences, look_ahead = 0, defaultdict(int), 3
+for x in data:
     if x in range(i, i + look_ahead+1):
         differences[x-i] += 1
     i = x
-differences[3]+=1
-print(differences[1] * differences[3]) # 2470
+print(differences[1] * (differences[3]+1)) # 2470
 
 # part 2
 counter_dict = Counter((0,))
 for i, row in enumerate(data):
-    counter_dict[row] = sum([counter_dict[y] for y in range(row-3, row) if y in data or y == 0])
+    counter_dict[row] = sum([counter_dict[y] for y in range(row-look_ahead, row) if y in data or y == 0])
 print(counter_dict[data[-1]]) # 1973822685184
